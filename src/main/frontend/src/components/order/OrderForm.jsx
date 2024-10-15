8af96ec9824559f31f2e435ed2fb78e0b22a8c20
jinwoong <rassayzsixt@gmail.com>
2024-10-15 17:00:55 +0900

    [241015] order 추가 중

--- src/main/frontend/src/components/order/OrderForm.jsx
+++ src/main/frontend/src/components/order/OrderForm.jsx
@@ -1,17 +1,17 @@
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+//import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { useNavigate } from "react-router-dom";
 
 const OrderForm = () => {
-  const [validated, setValidated] = useState(false);
+  //const [validated, setValidated] = useState(false);
 
   const navigate = useNavigate();
-
+  /*
   const fnSave = async () => {
-    let url = "/rest/pd/create";
+    let url = "/rest/od/create";
     let res = await fetch(url, {
       method: "POST",
       headers: {
@@ -23,12 +23,31 @@ const OrderForm = () => {
 
     if (data.resultCode === 200) {
       alert(data.resultMessage);
-      navigate("/admin/product/list");
+      navigate("/admin/order/form");
     }
   };
+*/
+  const handleSubmit = async (files) => {
+    console.log(files);
 
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
+    let url = "/rest/od/create";
+    let formData = new FormData();
+
+    formData.append("orderData", files);
+
+    let res = await fetch(url, {
+      method: "POST",
+      body: formData,
+    });
+
+    let data = await res.json();
+
+    if (data.resultCode === 200) {
+      alert(data.resultMessage);
+      navigate("/admin/order/form");
+    }
+
+    /*
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
@@ -38,10 +57,11 @@ const OrderForm = () => {
     }
 
     setValidated(true);
+    */
   };
 
   return (
-    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+    <Form noValidate>
       <Row>
         <Col md>
           <Form.Group className="form-box">
@@ -49,7 +69,9 @@ const OrderForm = () => {
             <Form.Control
               type="file"
               id="orderData"
+              accept=".xlsx, .xls, .csv"
               aria-describedby="pdFcNumDesc"
+              onChange={(e) => handleSubmit(e.target.files)}
               required
             />
             <Form.Text id="pdFcNumDesc" muted>
--- src/main/java/com/processmaker/mkprocs/modules/orders/controller/OrderController.java
+++ src/main/java/com/processmaker/mkprocs/modules/orders/controller/OrderController.java
@@ -2,18 +2,22 @@ package com.processmaker.mkprocs.modules.orders.controller;
 
 import com.processmaker.mkprocs.modules.orders.service.OrderService;
 import com.processmaker.mkprocs.utils.Result;
+import jakarta.servlet.annotation.MultipartConfig;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/rest/order")
+@RequestMapping("/rest/od")
 @RequiredArgsConstructor
 public class OrderController {
 
     public final OrderService orderService;
-
+    @PostMapping("create")
+    public Result create(@RequestPart("orderData") MultipartFile orderData) {
+        System.out.println(orderData);
+        return null;
+    }
     @GetMapping("list")
     public Result read() {
         return orderService.read();
