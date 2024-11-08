68ad14085f3772c8faea6c542342f1ef61396816
jinwoong <rassayzsixt@gmail.com>
2024-11-08 17:41:58 +0900

    [241108] react bootstrap -> mui로 변경

--- src/main/frontend/src/sections/Header.jsx
+++ src/main/frontend/src/sections/Header.jsx
@@ -1,3 +1,4 @@
+/*
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
@@ -5,91 +6,33 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from "react-router-dom";
 
 import { SiProcesswire } from "react-icons/si";
+*/
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { AppBar, Box, Toolbar, IconButton } from "@mui/material";
+import { AlternateEmail } from "@mui/icons-material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#1976d2",
+    },
+  },
+});
 
 const Header = () => {
   return (
-    <Navbar expand="lg" id="header">
-      <Navbar.Brand href="/" id="logo">
-        <SiProcesswire size={50} />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link>
-            <Link to="/" className="link-menu">
-              Home
-            </Link>
-          </Nav.Link>
-
-          {/*--Modules--*/}
-          <NavDropdown title="Modules" id="basic-nav-dropdown">
-            <NavDropdown.Item>
-              <Link to="/modules/guides" className="link-menu">
-                Guides
-              </Link>
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <Link to="/modules/form" className="link-menu">
-                Form
-              </Link>
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <Link to="/modules/list" className="link-menu">
-                List
-              </Link>
-            </NavDropdown.Item>
-          </NavDropdown>
-
-          {/*--Process--*/}
-          <NavDropdown title="Processes" id="basic-nav-dropdown">
-            <NavDropdown.Item>
-              <Link to="/processes/guides" className="link-menu">
-                Guides
-              </Link>
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <Link to="/processes/form" className="link-menu">
-                Form
-              </Link>
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <Link to="/processes/list" className="link-menu">
-                List
-              </Link>
-            </NavDropdown.Item>
-          </NavDropdown>
-
-          {/*--Admin--*/}
-          <NavDropdown title="Admin" id="basic-nav-dropdown">
-            <NavDropdown.Item>
-              <Link to="/admin/platform/list" className="link-menu">
-                Platform
-              </Link>
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <Link to="/admin/factory/list" className="link-menu">
-                Factory
-              </Link>
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <Link to="/admin/product/list" className="link-menu">
-                Product
-              </Link>
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <Link to="/admin/product/category/list" className="link-menu">
-                Category
-              </Link>
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <Link to="/admin/orders/form" className="link-menu">
-                Orders
-              </Link>
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <ThemeProvider theme={darkTheme}>
+      <AppBar position="static">
+        <Toolbar id="header">
+          <Box component="div" sx={{ flexGrow: 1 }} id="logo">
+            <IconButton size="xlarge" edge="start" color="inherit">
+              <AlternateEmail id="spinner" size="large" />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </ThemeProvider>
   );
 };
 
---
+++ src/main/resources/static/static/js/423.3d68ba1a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[423],{423:(t,e,n)=>{n.d(e,{qqM:()=>h});var a=n(3441);function h(t){return(0,a.k5)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M416 199.5h-91.4V64H187.4v135.5H96l160 158.1 160-158.1zM96 402.8V448h320v-45.2H96z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=423.3d68ba1a.chunk.js.map
